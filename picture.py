166ccf8ea9e36e7f93da97a44eec9f33c8dd7a52
aroldo <amunozro@unsa.edu.pe>
2023-06-04 23:33:25 -0500

    ejerc2b y rotate

--- picture.py
+++ picture.py
@@ -15,13 +15,22 @@ class Picture:
   def verticalMirror(self):
     """ Devuelve el espejo vertical de la imagen """
     vertical = []
-    #for value in self.img:
-  #  	vertical.append(value[::-1])
+    for value in self.img:
+      vertical.append(value[::-1])
     return vertical
 
   def horizontalMirror(self):
+    horizon=[]
+    n=len(self.img)
+    nn=len(self.img[0])
+    
+    for i in range(n):
+      horizon.append([])
+      for j in range(nn):
+        horizon[i].append(self.img[i][nn-1-j])
+    return horizon
     """ Devuelve el espejo horizontal de la imagen """
-    return Picture(None)
+    
 
   def negative(self):
     """ Devuelve un negativo de la imagen """
@@ -30,7 +39,7 @@ class Picture:
   def join(self, chess):
     """ Devuelve una nueva figura poniendo la figura del argumento 
         al lado derecho de la figura actual """
-    ny=len(self.img)
+    """ny=len(self.img)
     nx=len(self.img[0])
     nny=len(chess)
     nnx=len(chess[0])
@@ -38,12 +47,13 @@ class Picture:
     for j in range(0,nny):
       for i in range(0,nnx):
         nuevo[j]=nuevo[j]+chess[j][i]
-        
-    """nny=len(chess)
+        """
+    nny=len(chess)
     nuevo=self.img
     for j in range(0,nny):
         nuevo[j]=nuevo[j]+chess[j]
-    return nuevo"""
+        
+    return nuevo
 
 
   def up(self, chess):
@@ -61,15 +71,42 @@ class Picture:
     return Picture(None)
   
   def horizontalRepeat(self, n):
+    
+    horiz=self
+   
+    """
+    for i in range(0,n):
+      horiz.append(self.img)
+    return horiz
+    """
+    for i in range(0,n):
+      horiz.join(self.img)
+      
+    return horiz
+ 
+     
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
-
   def verticalRepeat(self, n):
-    return Picture(None)
+    verti=[]
+    #pri=self.img
+    for i in range(0,n):
+      verti=verti+self.img
+    return verti
+  
+    
 
   #Extra: Sólo para realmente viciosos 
   def rotate(self):
     """Devuelve una figura rotada en 90 grados, puede ser en sentido horario
     o antihorario"""
-    return Picture(None)
+    horizon=[]
+    n=len(self.img)
+    nn=len(self.img[0])
+    
+    for i in range(n):
+      horizon.append([])
+      for j in range(nn):
+        horizon[i].append(self.img[nn-1-j][i])
+    return horizon
+   
